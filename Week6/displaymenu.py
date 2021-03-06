8ed07ef4c5a42f7594744484aad051c7e1def292
Marcelo Reffatti <mreffatti@live.com>
2021-03-06 15:34:41 +0000

    Week 06

---
+++ Week6/displaymenu.py
@@ -0,0 +1,27 @@
+def displaymenu():
+    print("What  would you like to do? ")
+    print("\t(a) Add new student")
+    print("\t(v) View students")
+    print("\t(q) quit")
+    choice = input("Type your Letter (a/v/q): ").strip()
+
+    return choice
+
+def doAdd():
+     print("in adding")
+
+def doView():
+    print("in viewing")
+
+#main program
+choice = displaymenu()
+while(choice != 'q'):
+ # we could do this with lamda functions
+ # I am keeping this basic for the moment
+    if choice == 'a':
+        doAdd()
+    elif choice == 'v':
+        doView()
+    elif choice !='q':
+        print("\n\nplease select either a,v or q")
+    choice=displaymenu() 
---
+++ Week6/quiz01.py
@@ -0,0 +1,13 @@
+def yo(a):
+    return a * 2
+
+yo(3)
+
+# a. What will be printed out when this program is run?
+# Nothing.. 
+
+# b. What would you have to add to the program to get it to print out what the calling the function yo(3) returns?
+# Need to print out the output
+
+print(yo(3))
+
